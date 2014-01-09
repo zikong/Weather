4f45bc042d0a80b30a5abcb7a28fded1c2b22c17
jlzikong <zikong@appest.com>
2014-01-09 15:35:10 +0800

    Initial Commit

---
+++ Weather/ZKAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ZKAppDelegate.h
+//  Weather
+//
+//  Created by zikong on 14-1-9.
+//  Copyright (c) 2014年 zikong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZKAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Weather/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Weather
+//
+//  Created by zikong on 14-1-9.
+//  Copyright (c) 2014年 zikong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ZKAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ZKAppDelegate class]));
+    }
+}
